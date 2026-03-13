0689af23c04182956ae74bb74f3404a6950d9722
adubus <106705517+adubus@users.noreply.github.com>
2026-03-13 12:20:26 +0100

    Add files via upload

---
+++ public/supabase.js
@@ -0,0 +1,6 @@
+import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm'
+
+export const supabase = createClient(
+  'https://tfutyotziaqockkibnqz.supabase.co',
+  'sb_publishable_1I83K10sCCA5Ip2aJhaX4g_8faJ50r3'
+)
